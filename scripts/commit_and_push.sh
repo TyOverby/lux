11d2bcd3ce2b4b00f447247be66f1726133d026d
Ty Overby <ty@pre-alpha.com>
2021-07-04 12:48:39 -0400

    _

--- scripts/commit_and_push.sh
+++ scripts/commit_and_push.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 git add -A
 git commit -m '_'
 git rev-parse --short HEAD > "$SCRIPT_DIR/current_sha.txt"
+echo "export sha =\"$(cat $SCRIPT_DIR/current_sha.txt)\"" > "$SCRIPT_DIR/current_sha.js"
